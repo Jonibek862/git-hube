9d00503abed27b57bc628bd30c2ef66db31cff14
Jonibek862 <jonibekrizakulov0@gmail.com>
2024-12-04 10:26:56 +0500

    uyga vazifa

---
+++ uyga vazifa 1.3.py
@@ -0,0 +1,11 @@
+a=int(input("yoshingizni kiriting = "))
+if 0<a and a<=12:
+    print("bola")
+elif 13<=a and a<=17:
+    print("o'smir")
+elif 18<=a and a<=59:
+    print("kattalar")
+elif a>=60:
+    print("keksa")
+else:
+    print("bunaqa yosh mavjud emas")
---
+++ uyga vazifa 2.1.py
@@ -0,0 +1,5 @@
+a=int(input("butun sonni kiriting = "))
+if a%2==0:
+    print("juft")
+else:
+    print("toq")
